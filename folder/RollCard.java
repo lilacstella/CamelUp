c4bda4c24c2ef7de083f6e721598c25e960f0500
AbhinavVarre <37716743+AbhinavVarre@users.noreply.github.com>
2018-11-08 13:28:38 -0600

    Update RollCard.java

--- folder/RollCard.java
+++ folder/RollCard.java
@@ -1,4 +1,15 @@
 
-public class RollCard {
-
+public class RollCard 
+{
+	private String playerName;
+	
+	public RollCard(String n)
+	{
+		playerName = n;
+	}
+	
+	public String getPlayerName()
+	{
+		return playerName;
+	}
 }
