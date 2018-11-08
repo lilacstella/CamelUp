373b06598970a745f613b48c2f1793040b318f09
XxMingfei77xX <43825719+XxMingfei77xX@users.noreply.github.com>
2018-11-08 13:20:38 -0600

    Update Tile.java

--- folder/Tile.java
+++ folder/Tile.java
@@ -4,13 +4,27 @@ public class Tile
 {
 	private Camel camel;
 	private LinkedList<Camel> camels;
-	Trap trap;
+	private Trap trap;
 	public Tile() 
 	{
 		camels = new LinkedList<Camel>();
 	}
 	public Trap getTrap()
 	{
-		
+		return trap;
+	}
+	public void setTrap(Trap t)
+	{
+		trap = t;
+	}
+	public Trap RemoveTrap()
+	{
+		Trap t = trap;
+		trap = null;
+		return t;
+	}
+	public linkedlist<Camel> getCamel()
+	{
+		return camels;
 	}
 }
