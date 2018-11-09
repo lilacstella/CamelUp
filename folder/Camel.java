d8180ee1ff05ff140225221b8c662b77275e5386
XxMingfei77xX <43825719+XxMingfei77xX@users.noreply.github.com>
2018-11-09 13:17:06 -0600

    Update Camel.java

--- folder/Camel.java
+++ folder/Camel.java
@@ -8,13 +8,16 @@ public class Camel
 		color = c;
 		pos = 0;
 	}
+	//add num to pos
 	public int move(int num)
 	{
 		pos += num;
 		return pos;
 	}
+	//get the color of the camel
 	public String getCamelColor()
 	{
 		return color;
 	}
 }
+
