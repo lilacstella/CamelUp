5932caae5a1330e27bd2e534b85fd9279bbfddc7
XxMingfei77xX <43825719+XxMingfei77xX@users.noreply.github.com>
2018-11-12 13:39:24 -0600

    Update Tile.java

--- folder/Tile.java
+++ folder/Tile.java
@@ -1,6 +1,8 @@
 import java.util.LinkedList;
 
-public class Tile 
+//The Tile class is important because it represents the important feature of the desert tile in the sophisticated board game of camel up
+//Below are some insightful explanation of the function of each method.
+public class Tile  
 {
 	private LinkedList<Camel> camels;
 	private Trap trap;
@@ -13,19 +15,19 @@ public class Tile
 	{
 		return trap;
 	}
-	//set a trap
+	//put a trap on the tile
 	public void setTrap(Trap t)
 	{
 		trap = t;
 	}
-	//remove the trap
+	//remove the trap from the tile and returns it 
 	public Trap removeTrap()
 	{
 		Trap t = trap;
 		trap = null;
 		return t;
 	}
-	//get the linkedList of camels
+	//get the linkedList of camels for the graphics
 	public LinkedList<Camel> getCamel()
 	{
 		return camels;
