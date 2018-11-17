432d1a13d5aad70ea0247bd0c0ec7ee074a29cc4
PseudoCryptic <43301578+erichyang@users.noreply.github.com>
2018-11-17 17:54:33 -0600

    Deleted unused methods

--- Core/Tile.java
+++ Core/Tile.java
@@ -2,25 +2,20 @@ import java.util.LinkedList;
 
 //The Tile class is important because it represents the important feature of the desert tile in the sophisticated board game of camel up
 //Below are some insightful explanation of the function of each method.
-public class Tile  
+public class Tile
 {
 	private LinkedList<Camel> camels;
 	private Trap trap;
-	public Tile() 
+	public Tile()
 	{
 		camels = new LinkedList<Camel>();
-	}
-	//returns the trap
-	public Trap getTrap()
-	{
-		return trap;
 	}
 	//put a trap on the tile
 	public void setTrap(Trap t)
 	{
 		trap = t;
 	}
-	//remove the trap from the tile and returns it 
+	//remove the trap from the tile and returns it
 	public Trap removeTrap()
 	{
 		Trap t = trap;
