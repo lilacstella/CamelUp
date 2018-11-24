97c7910bca64fca11acf5caccd1e89cab7e0548c
PseudoCryptic <43301578+erichyang@users.noreply.github.com>
2018-11-24 09:00:27 -0600

    Camel move() is not used

--- src/Core/Camel.java
+++ src/Core/Camel.java
@@ -1,19 +1,10 @@
 public class Camel
 {
 	private String color;
-	private int pos;
 
 	public Camel(String c)
 	{
 		color = c;
-		pos = 0;
-	}
-
-	// add num to pos
-	public int move(int num)
-	{
-		pos += num;
-		return pos;
 	}
 
 	// get the color of the camel
