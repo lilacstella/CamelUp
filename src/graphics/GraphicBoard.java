e6db713593a3232258644eed4bce8c79625960d8
Sarim Aleem <sarimaleem99@gmail.com>
2018-12-05 18:01:56 -0600

    graphics dice and testing done
    adjust the x and y position of where to draw the dice where it says x: 800, y:800

--- src/graphics/GraphicBoard.java
+++ src/graphics/GraphicBoard.java
@@ -1,13 +1,15 @@
 package graphics;
 
 import core.CamelUp;
+import core.Dice;
 
 import javax.swing.*;
+import java.util.HashSet;
 import java.awt.*;
 
 public class GraphicBoard extends JPanel {
 
-    private CamelUp gameState = new CamelUp();
+    private static CamelUp gameState;
 
     public static void main(String[] args) {
         JFrame window = new JFrame("Camel Up");
@@ -16,14 +18,28 @@ public class GraphicBoard extends JPanel {
         window.setVisible(true);
         GraphicBoard board = new GraphicBoard();
         window.add(board);
+        gameState = new CamelUp();
+        gameState.roll();
+        gameState.roll();
+        gameState.roll();
     }
 
 
     public void paintComponent(Graphics graphics) {
         Graphics2D graphics2D = (Graphics2D)graphics;
+        drawDiceRolled(graphics2D, gameState.getRolled());
+        repaint();
     }
 
-    public void drawDiceRolled(Graphics2D graphics2D) {
+    public void drawDiceRolled(Graphics2D graphics2D, HashSet<Dice> diceRolled) {
+
+        int adjX = 0;
+        for (Dice dice : diceRolled) {
+            GraphicDice graphicDice = new GraphicDice(new Point(800 + adjX, 800), dice);
+            graphicDice.draw(graphics2D);
+            adjX += 125;
+
+        }
 
     }
 
--- src/graphics/GraphicDice.java
+++ src/graphics/GraphicDice.java
@@ -5,23 +5,38 @@ import core.Dice;
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class GraphicDice
 {
     private Point pos;
     private Dice dice;
+    private HashMap<String, Color> colorHashMap;
+
     public GraphicDice(Point point, Dice dice)
     {
         pos = point;
         this.dice = dice;
+        colorHashMap = new HashMap<>();
+        colorHashMap.put("yellow", Color.yellow);
+        colorHashMap.put("green", Color.green);
+        colorHashMap.put("blue", Color.blue);
+        colorHashMap.put("orange", Color.orange);
+        colorHashMap.put("white", Color.white);
+
+
     }
 
     public void draw(Graphics2D graphics2D) {
-        graphics2D.setPaint(Color.BLACK);
 
         int x = pos.x;
         int y = pos.y;
+
+        graphics2D.setColor(colorHashMap.get(dice.getColor()));
+        graphics2D.fill(new Rectangle(x, y, 100, 100));
+        graphics2D.setPaint(Color.BLACK);
+
 
 
         switch (dice.getDieFace()) {
