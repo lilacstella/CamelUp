dab663a357fb86c7c81df185ab427c4e5dffe92f
Sarim Aleem <sarimaleem99@gmail.com>
2018-12-19 15:33:03 -0600

    test

--- src/graphics/GraphicBoard.java
+++ src/graphics/GraphicBoard.java
@@ -433,7 +433,7 @@ public class GraphicBoard extends JPanel implements MouseListener
 
 	private void drawTitle(Graphics2D graphics2D) {
 		graphics2D.setColor(new Color(212,175,55));
-		graphics2D.setFont(new Font("Comic Sans MS", Font.ITALIC., 100));
+		graphics2D.setFont(new Font("Comic Sans MS", Font.ITALIC, 100));
 		graphics2D.drawString("Camel Up", 625, 100);
 	}
 
